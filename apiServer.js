b7e018013f726e923518bd8b9d3a1a55b20f7d4f
Christian Armstrong <christian@medpactech.com>
2021-02-15 10:57:02 -0500

    Saving JSONB to database.

--- apiServer.js
+++ apiServer.js
@@ -20,10 +20,11 @@ app.get('/', (req, res) => res.json({ message: 'Hello Hubbsly' }));
 
 // connect to the db
 const db = require('./app/models');
-const { sequelize } = require('./app/models');
+const { sequelize, Sequelize } = require('./app/models');
 db.sequelize.sync();
 // db.sequelize.sync({ force: true }).then(() => {
 //   console.log('Drop and re-sync db.');
+//   initial();
 // });
 
 // test connection
@@ -36,7 +37,82 @@ sequelize
     console.error('Unable to connect to the databse:', err);
   });
 
+// Temp User - tie to user service later
+const User = sequelize.define(
+  'user',
+  {
+    // attributes
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      // allowNull defaults to true
+    },
+    address: {
+      type: Sequelize.JSONB,
+    },
+  },
+  {
+    // options
+  }
+);
+
+User.sync({ force: true });
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>
   console.log(`Hubbsly API is running on port ${PORT}...`)
 );
+
+function initial() {
+  User.create({
+    id: 1,
+    firstName: 'Fred',
+    lastName: 'Flinstone',
+    address: {
+      address1: '345 Cave Stone Road',
+      city: 'Bedrock',
+      state: 'Stone Age America',
+      zipCode: '88888',
+      county: 'Cobblestone County',
+    },
+  });
+  User.create({
+    id: 2,
+    firstName: 'Wilma',
+    lastName: 'Flinstone',
+    address: {
+      address1: '345 Cave Stone Road',
+      city: 'Bedrock',
+      state: 'Stone Age America',
+      zipCode: '88888',
+      county: 'Cobblestone County',
+    },
+  });
+  User.create({
+    id: 3,
+    firstName: 'Barney',
+    lastName: 'Rubble',
+    address: {
+      address1: '347 Cave Stone Road',
+      city: 'Bedrock',
+      state: 'Stone Age America',
+      zipCode: '88888',
+      county: 'Cobblestone County',
+    },
+  });
+  User.create({
+    id: 4,
+    firstName: 'Betty',
+    lastName: 'Rubble',
+    address: {
+      address1: '347 Cave Stone Road',
+      city: 'Bedrock',
+      state: 'Stone Age America',
+      zipCode: '88888',
+      county: 'Cobblestone County',
+    },
+  });
+}
